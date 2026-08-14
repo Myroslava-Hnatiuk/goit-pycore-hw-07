a80deac74b53ccf0490ac47b72461881e90f137f
Myroslava-Hnatiuk <myroslava.hnatiuk@ukr.net>
2026-08-14 17:16:26 +0100

    Init commit

---
+++ classes.py
@@ -0,0 +1,119 @@
+from collections import UserDict
+import re
+from datetime import datetime
+from handlers import get_upcoming_birthdays
+
+phone_regex = r'^\d{10}$'
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value: str):
+        self.value = value
+
+class Phone(Field):
+    def __init__(self, value: str):
+        if not re.match(phone_regex, value):
+            raise ValueError("Invalid phone number format")
+        self.value = value
+
+class Birthday(Field):
+    def __init__(self, value):
+        try:
+            parsed = datetime.strptime(value, "%d.%m.%Y")
+            self.value = parsed
+        except ValueError:
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for i, p in enumerate(self.phones):
+            if p.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+            else:
+                None
+
+    def add_birthday(self, birthday):
+        try:
+            self.birthday = Birthday(birthday)
+            return True
+        except ValueError as e:
+            print(f"Invalid birthday for {self.name.value}: {e}")
+            return False
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}, birthday: {self.birthday}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def show_upcoming_birthdays(self):
+        return get_upcoming_birthdays(self.data)
+
+
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+john_record.add_birthday('20.08.2000')
+
+# # Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+jane_record.add_birthday('15.09.1995')
+book.add_record(jane_record)
+
+# # Виведення всіх записів у книзі
+for name, record in book.data.items():
+    print(record)
+
+print("\nUpcoming birthdays:")
+for user in book.show_upcoming_birthdays():
+    print(user)
+
+# # Знаходження та редагування телефону для John
+# john = book.find("John")
+# john.edit_phone("1234567890", "1112223333")
+
+# print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# # Пошук конкретного телефону в записі John
+# found_phone = john.find_phone("5555555555")
+# print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+# # Видалення запису Jane
+# book.delete("Jane")
---
+++ handlers.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+
+from classes import Record
+
+def get_congratulations_date(birthday: datetime) -> datetime:
+    if birthday.weekday() == 5:  # Saturday
+        return birthday + timedelta(days=2) # If the birthday falls on a Saturday, the congratulations date is set to the following Monday (2 days later).
+    if birthday.weekday() == 6:  # Sunday
+        return birthday + timedelta(days=1) # If the birthday falls on a Sunday, the congratulations date is set to the following Monday (1 day later).
+    return birthday
+
+
+def build_birthday_record(user: dict[str, str], birthday: datetime) -> dict[str, str]:
+    congratulation_date = get_congratulations_date(birthday)
+    return {
+        "name": user["name"],
+        "congratulation_date": congratulation_date.strftime("%Y.%m.%d"),
+    }
+
+def get_upcoming_birthdays(users: dict[str, Record]) -> list[dict[str, str]]:
+    today = datetime.today().date() # Get today's date as a date object
+    upcoming_birthdays = [] # List to store users with upcoming birthdays
+
+    for user in users.values(): # Iterate through each user in the dictionary
+        birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date() # Convert the birthday string to a date object
+        birthday_this_year = datetime(
+            today.year,
+            birthday.month,
+            birthday.day
+        ).date() # Create a date object for the user's birthday in the current year
+       
+        if birthday_this_year < today:
+            birthday_this_year = birthday_this_year.replace(year=today.year + 1)
+
+        date_diff = (birthday_this_year - today).days # Calculate the difference in days between the birthday and today
+        if 0 <= date_diff <= 7: # Include birthdays from today through the next 7 days inclusive
+            upcoming_birthdays.append(build_birthday_record(user, birthday_this_year))
+
+    return upcoming_birthdays
